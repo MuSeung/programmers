0099ef3a98d895cbd05e898f78f7cece17493eab
MuSeung <peter0757@naver.com>
2023-07-04 09:16:40 +0900

    .

---
+++ src/d3/lv1ex17682.java
@@ -0,0 +1,46 @@
+package d3;
+
+public class lv1ex17682 {
+
+    public int solution(String dartResult) {
+        int answer = 0;
+        int count = 0;
+        int[] number = new int[3];
+        for (int i = 0; i < 12; i++) {
+            if (dartResult.charAt(i) <= '9' && dartResult.charAt(i) >= '0') {
+                number[count] = dartResult.charAt(i) - '0';
+                if (dartResult.charAt(i + 1) == '0') {
+                    number[count] = 10;
+                    i++;
+                }
+            }
+
+            else if (dartResult.charAt(i) == 'D') {
+                number[count] = number[count] * number[count];
+            }
+            else if (dartResult.charAt(i) == 'T') {
+                number[count] = number[count] * number[count] * number[count];
+            }
+
+            else if (dartResult.charAt(i) == '*') {
+                number[count] = number[count] * 2;
+                if(count>0){
+                    number[count - 1] = number[count - 1] * 2;
+                }
+            }
+            else if (dartResult.charAt(i) == '#') {
+                number[count] = -number[count];
+            }
+
+            if(i+1 >= dartResult.length()){
+                break;
+            }
+            if(dartResult.charAt(i+1) <= '9' && dartResult.charAt(i+1) >= '0'){
+                count++;
+            }
+        }
+        //System.out.println("1: "+number[0]+" 2: "+number[1]+" 3: "+number[2]);
+        answer = number[0]+number[1]+number[2];
+        return answer;
+    }
+}
