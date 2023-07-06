2aebfab8f0fa2848351f867b74db3b87d2e2991e
MuSeung <peter0757@naver.com>
2023-07-06 23:15:52 +0900

    .

---
+++ src/d5/lv1ex86051.java
@@ -0,0 +1,12 @@
+package d5;
+
+public class lv1ex86051 {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int number : numbers){
+            answer -= number;
+        }
+        return answer;
+    }
+
+}
